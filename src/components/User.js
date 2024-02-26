d0203b1c8c815b4243f0e0e5afe87b21d41c79c6
chandranshh <chandranshyay@gmail.com>
2024-02-26 23:45:58 +0530

    made ui changes

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import defaultProfileImage from '../images/user.png';
+import profileImage from "../images/user.png";
+import BG_main from "../images/BG_main.png"; // Import background image
 import './styles.css'; // Import CSS file for styling
 
 const User = (props) => {
@@ -32,52 +34,58 @@ const User = (props) => {
     };
 
     return (
-        <div className="parent-container">
-            <div className="user-profile-box">
-                <div><h2>USER PROFILE</h2></div>
-                {editMode ? (
-                    <form onSubmit={handleFormSubmit}>
-                        {/* Input fields for editing data */}
-                        <label className="input-label">First Name: <input type="text" name="firstName" value={userData.firstName} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">Last Name: <input type="text" name="lastName" value={userData.lastName} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">Position: <input type="text" name="position" value={userData.position} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">School: <input type="text" name="school" value={userData.school} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">Department: <input type="text" name="department" value={userData.department} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">Phone: <input type="text" name="phone" value={userData.phone} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        <label className="input-label">Email: <input type="text" name="email" value={userData.email} onChange={handleInputChange} /></label>
-                        <br /><br />
-                        {/* Submit button with inline styling */}
-                        <button className="submit-button" type="submit">Submit</button>
-                    </form>
-                ) : (
-                    <>
-                        {/* Display fetched names */}
-                        <label>First Name: {userData.firstName}</label>
-                        <br /><br />
-                        <label>Last Name: {userData.lastName}</label>
-                        <br /><br />
-                        <label>Position: {userData.position}</label>
-                        <br /><br />
-                        <label>School: {userData.school}</label>
-                        <br /><br />
-                        <label>Department: {userData.department}</label>
-                        <br /><br />
-                        <label>Phone: {userData.phone}</label>
-                        <br /><br />
-                        <label>Email: {userData.email}</label>
-                        <br /><br />
-                        {/* Edit button with inline styling */}
-                        <button className="edit-button" onClick={() => setEditMode(true)}>Edit</button>
-                    </>
-                )}
-                {/* Logout button */}
-                <button className="logout-button" type="button" onClick={() => console.log("Logout")}>Logout</button>
+        <div className="user-container" style={{backgroundImage: `url(${BG_main})`, backgroundSize: 'cover'}}>
+            <div className="orange_overlay">
+            <div className="parent-container">
+                <div className="user-profile-box">
+                    <div><h2>USER PROFILE</h2></div>
+                    {editMode ? (
+                        <form onSubmit={handleFormSubmit}>
+                            {/* Input fields for editing data */}
+                            <img className="user_img" src={profileImage} alt="profileImage" />
+                            <label className="input-label">First Name: <input type="text" name="firstName" value={userData.firstName} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">Last Name: <input type="text" name="lastName" value={userData.lastName} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">Position: <input type="text" name="position" value={userData.position} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">School: <input type="text" name="school" value={userData.school} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">Department: <input type="text" name="department" value={userData.department} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">Phone: <input type="text" name="phone" value={userData.phone} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            <label className="input-label">Email: <input type="text" name="email" value={userData.email} onChange={handleInputChange} /></label>
+                            <br /><br />
+                            {/* Submit button with inline styling */}
+                            <button className="submit-button" type="submit">Submit</button>
+                        </form>
+                    ) : (
+                        <>
+                            {/* Display fetched names */}
+                            <img className="user_img" src={profileImage} alt="profileImage" />
+                            <label>First Name: {userData.firstName}</label>
+                            <br /><br />
+                            <label>Last Name: {userData.lastName}</label>
+                            <br /><br />
+                            <label>Position: {userData.position}</label>
+                            <br /><br />
+                            <label>School: {userData.school}</label>
+                            <br /><br />
+                            <label>Department: {userData.department}</label>
+                            <br /><br />
+                            <label>Phone: {userData.phone}</label>
+                            <br /><br />
+                            <label>Email: {userData.email}</label>
+                            <br /><br />
+                            {/* Edit button with inline styling */}
+                            <button className="edit-button" onClick={() => setEditMode(true)}>Edit</button>
+                        </>
+                    )}
+                    {/* Logout button */}
+                    <button className="logout-button" type="button" onClick={() => console.log("Logout")}>Logout</button>
+                </div>
+            </div>
             </div>
         </div>
     );
